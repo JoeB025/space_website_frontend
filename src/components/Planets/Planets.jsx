1dd96fb5f63c73ec9a4f32ce9a8e09682ce23b31
Joseph Scott-Bentley <josephscott-bentley@joes-mbp-2.home>
2024-03-27 15:28:37 +0000

    styling update to planets page

--- src/components/Planets/Planets.jsx
+++ src/components/Planets/Planets.jsx
@@ -1,169 +1,155 @@
 import "./Planets.css";
 import { getPlanets, getImages } from "../../utils";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Planets() {
-
-  const [planetList, setPlanetList] = useState([])
-  const [imageList, setImageList] = useState([])
+  const [planetList, setPlanetList] = useState([]);
+  const [imageList, setImageList] = useState([]);
 
   useEffect(() => {
     getPlanets().then((response) => {
-      setPlanetList(response.data.planet)
-      console.log(response.data.planet[0].name)
+      setPlanetList(response.data.planet);
+      console.log(response.data.planet[0].name);
     });
   }, []);
 
   useEffect(() => {
     getImages().then((response) => {
-      setImageList(response.data.image)
-      console.log(response.data.image)
+      setImageList(response.data.image);
     });
   }, []);
 
-
-
   return (
     <>
-    <div className="container-planets">
-    <p className="planet-page-title">Welcome to the Planets Page!</p>
+      <div className="container-planets">
+        <h1 className="planet-page-title">Welcome to the Planets Page!</h1>
+        <section className="planet-img-top-section">
+          <ul className="planet-image-list-top">
+            {planetList.map((img) => {
+              let className = "planet-order-images-top"; // Default className
+              if (img.name === "Mercury") {
+                className += " mercury-image-top";
+              } else if (img.name === "Venus") {
+                className += " venus-image-top";
+              } else if (img.name === "Earth") {
+                className += " earth-image-top";
+              } else if (img.name === "Mars") {
+                className += " mars-image-top";
+              } else if (img.name === "Jupiter") {
+                className += " jupiter-image-top";
+              } else if (img.name === "Saturn") {
+                className += " saturn-image-top";
+              } else if (img.name === "Uranus") {
+                className += " uranus-image-top";
+              } else if (img.name === "Neptune") {
+                className += " neptune-image-top";
+              } else if (img.name === "Pluto") {
+                className += " pluto-image-top";
+              }
 
-<section className="planet-img-top-section">
-  <ul className="planet-image-list-top">
-    {planetList.map((img) => {
-      let className = 'planet-order-images-top'; // Default className
-      if (img.name === 'Mercury') {
-        className += ' mercury-image-top';
-      } else if (img.name === 'Venus') {
-        className += ' venus-image-top';
-      } else if (img.name === 'Earth') {
-        className += ' earth-image-top';
-      } else if (img.name === 'Mars') {
-        className += ' mars-image-top';
-      } else if (img.name === 'Jupiter') {
-        className += ' jupiter-image-top';
-      } else if (img.name === 'Saturn') {
-        className += ' saturn-image-top';
-      } else if (img.name === 'Uranus') {
-        className += ' uranus-image-top';
-      } else if (img.name === 'Neptune') {
-        className += ' neptune-image-top';
-      } else if (img.name === 'Pluto') {
-        className += ' pluto-image-top';
-      }
-      
-      if (img.name !== 'Sun') {
-      return (
-        <h1 key={img.name}>
-          <li>
-            <div className="planet-image-container-top">
-              <img
-                src={img.img_url}
-                className={className}
-                alt={img.name}
-              />
-            </div>
-          </li>
-        </h1>
-      );
-      }
-    })}
-  </ul>
-</section>
+              if (img.name !== "Sun") {
+                return (
+                  <h2 key={img.name}>
+                    <li>
+                      <div className="planet-image-container-top">
+                        <img
+                          src={img.img_url}
+                          className={className}
+                          alt={img.name}
+                        />
+                      </div>
+                    </li>
+                  </h2>
+                );
+              }
+            })}
+          </ul>
+        </section>
 
+        <section>
+          <h3 className="sub-heading">Info on individual planets:</h3>
+          <ul className="planet-list">
+            {planetList.map((planet) => {
+              if (planet.name !== "Sun" && planet.name !== "Pluto") {
+                return (
+                  <h4 key={planet.name} className="planet-string">
+                    <li>
+                      <div className="planet-main-container">
+                        <div className="planet-info-container">
+                          <p className="planet-name">{planet.name}</p>
+                          <p className="planet-type">
+                            {planet.name} is a {planet.planet_type} planet. It
+                            is {planet.distance_from_sun} million km from the
+                            Sun.
+                          </p>
+                          <p className="number-of-moons">
+                            Number of Moons: {planet.number_of_moons}
+                          </p>
+                        </div>
 
+                        <div className="article-image-container">
+                          <img
+                            src={planet.img_url}
+                            className="planet-image"
+                            alt={planet.name}
+                          />
+                        </div>
+                      </div>
+                    </li>
+                  </h4>
+                );
+              }
+            })}
+          </ul>
+        </section>
 
+        <section className="planet-img-bottom-section">
+          <ul className="planet-image-list-bottom">
+            {planetList.map((img) => {
+              let className = "planet-order-images-bottom"; // Default className
+              if (img.name === "Mercury") {
+                className += " mercury-image-bottom";
+              } else if (img.name === "Venus") {
+                className += " venus-image-bottom";
+              } else if (img.name === "Earth") {
+                className += " earth-image-bottom";
+              } else if (img.name === "Mars") {
+                className += " mars-image-bottom";
+              } else if (img.name === "Jupiter") {
+                className += " jupiter-image-bottom";
+              } else if (img.name === "Saturn") {
+                className += " saturn-image-bottom";
+              } else if (img.name === "Uranus") {
+                className += " uranus-image-bottom";
+              } else if (img.name === "Neptune") {
+                className += " neptune-image-bottom";
+              } else if (img.name === "Sun") {
+                className += " sun-image-bottom";
+              } else if (img.name === "Pluto") {
+                className += " pluto-image-bottom";
+              }
 
-
-
-      <section>
-        <p>Info on individual planets:</p>
-      <ul className="planet-list">
-      {planetList.map((planet) => {
-        if (planet.name !== 'Sun' && planet.name !== 'Pluto') {
-        return (
-
-          <h2 key={planet.name} className="planet-string">
-
-            <li>
-              <div className="planet-main-container">
-                <div className="planet-info-container">
-                  <p className="planet-name">{planet.name}</p>
-                  <p className="planet-type">{planet.name} is a {planet.planet_type} planet.</p>
-                  <p className="distance_from_sun">{planet.name} is {planet.distance_from_sun} million km from the Sun.</p>
-                  <p className="number-of-moons">{planet.name} has {planet.number_of_moons} moons.</p>
-                </div>
-                <div className="article-image-container">
-                    <img
-                      src={planet.img_url}
-                      className="planet-image"
-                      alt={planet.name}
-                    />
-                  </div>
-              </div>
-            </li>
-
-          </h2>
-        )
-        }
-      })}
-
-
-      </ul>
-      
-      
-      </section>
-
-      <section className="planet-img-bottom-section">
-  <ul className="planet-image-list-bottom">
-    {planetList.map((img) => {
-      let className = 'planet-order-images-bottom'; // Default className
-      if (img.name === 'Mercury') {
-        className += ' mercury-image-bottom';
-      } else if (img.name === 'Venus') {
-        className += ' venus-image-bottom';
-      } else if (img.name === 'Earth') {
-        className += ' earth-image-bottom';
-      } else if (img.name === 'Mars') {
-        className += ' mars-image-bottom';
-      } else if (img.name === 'Jupiter') {
-        className += ' jupiter-image-bottom';
-      } else if (img.name === 'Saturn') {
-        className += ' saturn-image-bottom';
-      } else if (img.name === 'Uranus') {
-        className += ' uranus-image-bottom';
-      } else if (img.name === 'Neptune') {
-        className += ' neptune-image-bottom';
-      } else if (img.name === 'Sun') {
-        className += ' sun-image-bottom';
-      } else if (img.name === 'Pluto') {
-        className += ' pluto-image-bottom';
-      } 
-
-      return (
-        <h1 key={img.name}>
-          <li>
-            <div className="planet-image-container-bottom">
-              <img
-                src={img.img_url}
-                className={className}
-                alt={img.name}
-              />
-            </div>
-          </li>
-        </h1>
-      );
-    })}
-  </ul>
-</section>
-
-    </div>
+              return (
+                <h5 key={img.name}>
+                  <li>
+                    <div className="planet-image-container-bottom">
+                      <img
+                        src={img.img_url}
+                        className={className}
+                        alt={img.name}
+                      />
+                    </div>
+                  </li>
+                </h5>
+              );
+            })}
+          </ul>
+        </section>
+      </div>
     </>
-  )
+  );
 }
-
-
 
 /*
 
