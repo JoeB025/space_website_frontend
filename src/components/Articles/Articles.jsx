bf803eefa7306d067157f6af874e43efb0123ad0
Joseph Scott-Bentley <josephscott-bentley@joes-mbp-2.home>
2024-04-02 13:05:23 +0100

    Article page Styling update

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -1,72 +1,95 @@
 import "./Articles.css";
+import "./Links.css";
+import "./DropDown.css";
 import { getArticles } from "../../utils";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Articles() {
+  const [articleName, setArticleName] = useState([]);
 
-  const [articleName, setArticleName] = useState([])
+  useEffect(() => {
+    getArticles().then((response) => {
+      setArticleName(response.data.article);
+      console.log(response.data.article);
+    });
+  }, []);
 
-useEffect(() => {
-  getArticles().then((response) => {
-    setArticleName(response.data.article)
-    console.log(response.data.article)
-  });
-}, []);
+  return (
+    <>
+      <div className="container">
+        <section>
+          <h1 className="article-page-title">Articles</h1>
 
-
-return (
-  <>
-  <div className="container">
-    <section>
-      <p className="article-page-title">Welcome to the Articles Page!</p>
-
-      <ul className="article-list">
-        {articleName.map((article) => {
-          return (
-            <h1 key={article.title} className="article-container">
-            <li>
-              <div className="main-container">
-              <div className="'info-container">
-              <h2 className="article-title">{article.title}</h2>
-              <p className="article-author">Author: {article.author}</p>
-              <p className="article-topic">Topic: {article.topic}</p>
-              <p className="article-votes">Votes: {article.votes}</p>
-              <p className="article-comment-count">Comment Count:{article.number_of_comments}</p>
-              <p className="article-date">Created: {article.created_at}</p>
+          <div className="article-content-container">
+            <div className="art-dropdown">
+              <button className="art-dropdown-button">
+                <i>Select Your Topic:</i>
+              </button>
+              <div className="art-dropdown-content">
+                <Link to="planets">filter all topics...</Link>
+                <Link to="/planets/1">Topic 1</Link>
+                <Link to="/planets/2">Topic 2</Link>
+                <Link to="/planets/3">Topic 3</Link>
               </div>
-              <div className="article-image-container">
-                    <img
-                      src={article.article_img_url}
-                      className="article-image"
-                      alt={article.title}
-                    />
-                  </div>
-                  </div>
-                  <Link
-                    to={`/articles/${article.article_id}`}
-                    className="articleLink"
-                  >
-                    Link to article page
-                  </Link>
-            </li>
-            </h1>
-          )
-        })}
-      </ul>
-    </section>
+            </div>
 
-    <div className="all-article-bottom-links">
-      <Link to="/" className="articles-back-home">
-            Home
-          </Link> 
+            <ul className="article-list">
+              {articleName.map((article) => {
+                return (
+                  <h2 key={article.title} className="article-container">
+                    <li>
+                      <div className="main-container">
+                        <div className="article-image-container">
+                          <img
+                            src={article.article_img_url}
+                            className="article-image"
+                            alt={article.title}
+                          />
+                        </div>
+                        <div className="'info-container">
+                          <Link
+                            to={`/articles/${article.article_id}`}
+                            className="article-title"
+                          >
+                            {article.title}
+                          </Link>
+                        </div>
+                      </div>
+
+                      <div className="topic-author-container">
+                        <div className="author-container">
+                          <p className="article-author-desc">
+                            Author:{" "}
+                            <span className="article-author-name">
+                              {article.author}
+                            </span>
+                          </p>
+                        </div>
+                        <div className="topic-container">
+                          <p className="article-topic-desc">
+                            Topic:{" "}
+                            <span className="article-topic-name">
+                              {article.topic}
+                            </span>
+                          </p>
+                        </div>
+                      </div>
+                      <p className="article-votes">💙 {article.votes}</p>
+                    </li>
+                  </h2>
+                );
+              })}
+            </ul>
           </div>
+        </section>
 
-
-  </div>
-     
-  </>
-)
-
+        <div className="all-article-bottom-links">
+          <Link to="/" className="articles-back-home">
+            Home
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
-
