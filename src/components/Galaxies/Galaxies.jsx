dbdda0fbefb599add72d9dc450ca917f65adabd1
Joseph Scott-Bentley <josephscott-bentley@joes-mbp-2.home>
2024-04-02 14:01:13 +0100

    Galaxies Page styling updated. Still incomplete.

--- src/components/Galaxies/Galaxies.jsx
+++ src/components/Galaxies/Galaxies.jsx
@@ -1,67 +1,78 @@
-import "./Galaxies.css"; 
-import "./Links.css"
-import { getGalaxies } from "../../utils"
-import { useEffect, useState } from "react"
+import "./Galaxies.css";
+import "./Links.css";
+import "./PageContainer.css";
+import { getGalaxies } from "../../utils";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function GalaxyData() {
-  const [galaxyList, setGalaxyList] = useState([])
+  const [galaxyList, setGalaxyList] = useState([]);
 
   useEffect(() => {
     getGalaxies().then((response) => {
-      setGalaxyList(response.data.galaxy)
-      console.log(response.data)
-    })
-  }, [])
+      setGalaxyList(response.data.galaxy);
+      console.log(response.data);
+    });
+  }, []);
 
+  return (
+    <>
+      <h1 className="galaxies-header">Galaxies</h1>
 
+      <div className="galaxies-container">
+        <ul className="galaxies-data-container">
+          {galaxyList.map((galaxy) => {
+            return (
+              <li key={galaxy.galaxy_id}>
+                <div className="galaxy-containers">
+                  <h2 className="galaxy-title">{galaxy.galaxy_name}</h2>
 
-return (
-  <>
-  <h1>Welcome to the Galaxies Page</h1>
+                  <div className="galaxy-top-section">
+                    <div className="galaxy-data-container">
+                      <p className="galaxy-constellation-text">Constellation: {galaxy.constellation}</p>
+                      <p className="galaxy-distance-text">Distance from earth: {galaxy.distance_from_earth}</p>
+                      <p className="galaxy-size-text">Size: {galaxy.size}</p>
+                      <p className="galaxy-type-text">Type: {galaxy.type}</p>
+                      <p className="galaxy-star-count-text">Number of stars: {galaxy.number_of_stars}</p>
+                    </div>
+                    <div className="general-info-container">
+                      <h2 className="about-heading">About</h2>
+                      <p>{galaxy.general_info}</p>
+                    </div>
+                  </div>
 
-  <div className="galaxies-container">
+                  <div className="galaxy-mid-section">
+                    <div className="obs-history-container">
+                      <h3 className="observation-heading">Observation History</h3>
+                      <p>{galaxy.observation_history}</p>
+                    </div>
+                    <div className="galaxy-img-container">
+                      <img
+                        src={galaxy.img_url}
+                        className="galaxy-image"
+                        alt={galaxy.galaxy_name}
+                      />
+                    </div>
+                  </div>
 
+              <div className="galaxy-bottom-section">
+                  <h4 className="timeline-heading">
+                    Timeline of {galaxy.galaxy_name}
+                  </h4>
+                  <p className="galaxy-timeline-text">{galaxy.timeline}</p>
+                </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
 
-<ul className="galaxies-data-container">
-  {galaxyList.map((galaxy) => {
-    return (
-      <li key={galaxy.galaxy_id}>
-
-        <div className="galaxy-containers">
-   
-            <h2 className="galaxy-title">{galaxy.galaxy_name}</h2>
-            <p>Constellation{galaxy.constellation}</p>
-            <p>Distance from earth: {galaxy.distance_from_earth}</p>
-            <p>Size: {galaxy.size}</p>
-            <p>Type: {galaxy.type}</p>
-            <p>General Info: {galaxy.general_info}</p>
-            <p>Number of stars: {galaxy.number_of_stars}</p>
-            <p>{galaxy.visible_galactic_center}</p>
-            <p>Observation History: {galaxy.observation_history}</p>
-            <p>Timeline: {galaxy.timeline}</p>
-            <img
-                      src={galaxy.img_url}
-                      className="galaxy-image"
-                      alt={galaxy.galaxy_name}
-                    />
-            
-            </div>
-    
-      </li>
-    )
-  })}
-</ul>
-  
-  
-  <div className="galaxies-bottom-links">
-      <Link to="/" className="galaxies-back-home">
+        <div className="galaxies-bottom-links">
+          <Link to="/" className="galaxies-back-home">
             Home
-          </Link> 
-          </div>
-    </div>
-  </>
-)
-
-
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
